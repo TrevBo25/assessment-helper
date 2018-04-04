927c87e32f3338f359eaae165582e9774fa3080c
Trevbo25 <trevorbrown25@gmail.com>
2018-04-04 11:18:45 -0700

    made the search less twitchy

--- src/components/AddStudentModal.js
+++ src/components/AddStudentModal.js
@@ -9,23 +9,28 @@ export default class AddStudentModal extends Component {
       term: '',
       results: []
     }
+
+    this.doSearch = this.doSearch.bind(this)
   }
 
-  componentDidUpdate(){
-    console.log(this.state.term)
-    axios.get(`/api/search?term=${this.state.term}`)
-      .then(response => {
-        console.log(response.data)
-        this.setState(state => ({results:response.data}))
-      })
+  doSearch(){
+    if(this.state.term){
+      axios.get(`/api/search?term=${this.state.term}`)
+        .then(response => {
+          console.log(response.data)
+          this.setState(state => ({results:response.data}))
+        })
+    }
   }
 
   render(){
+    var display = this.state.results.filter((e, i) => i < 5 && this.state.term && e.username[0].toLowerCase() === this.state.term[0].toLowerCase() && e.username.toLowerCase().includes(this.state.term.toLowerCase()) ? true : false)
+
     return(
       <div>
-        <input onChange={e => this.setState({term: e.target.value})} placeholder="Please enter the students username" value={this.state.term} />
+        <input onChange={e => this.setState({term: e.target.value}, this.doSearch())} placeholder="Please enter the students username" value={this.state.term} />
         <ul>
-          {this.state.results.map(e => (<li onClick={() => this.props.submit(e)}>{e.username}</li>))}
+          {display.map(e => (<li onClick={() => this.props.submit(e)}>{e.username}</li>))}
         </ul>
       </div>
     )
